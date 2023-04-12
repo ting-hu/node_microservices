ab615a41e38ab5e832f4891094410547401a8179
ting-hu <tinghu453@gmail.com>
2023-04-12 00:24:21 +0000

    Tried out fastify and Docker

---
+++ packages/settings-api/plugins/database.js
@@ -0,0 +1,26 @@
+import fp from 'fastify-plugin'
+
+export default fp(async function(fastify,opts){
+  fastify.decorate('db',new MockDatabase())
+})
+
+
+class MockDatabase {
+  constructor() {
+    this.db = {};
+  }
+
+  async saveSettings(userId, settings) {
+    await this.#delay();
+    this.db[userId] = settings;
+  }
+
+  async getSettings(userId) {
+    await this.#delay();
+    return this.db[userId];
+  }
+
+  async #delay() {
+    return new Promise(resolve => setTimeout(resolve, 10));
+  }
+}
---
+++ packages/settings-api/routes/settings/index.js
@@ -0,0 +1,28 @@
+const putOptions = {
+  schema: {
+    body: {
+      type: 'object',
+      properties: {
+        sides: { type: 'number' }
+      }
+    }
+  }
+}
+
+export default async function (fastify, opts) {
+  fastify.put('/:userId', putOptions,async function (request, reply) {
+    request.log.info(`Saving settings for user ${request.params.userId}`);
+    await fastify.db.saveSettings(request.params.userId, request.body);
+    reply.code(204);
+  });
+
+  fastify.get('/:userId', async function (request, reply) {
+    request.log.info(`Retrieving settings for user ${request.params.userId}`);
+    const settings = await fastify.db.getSettings(request.params.userId);
+    if (settings) {
+      return settings;
+    }
+    return { sides: 6 };
+  });
+}
+
